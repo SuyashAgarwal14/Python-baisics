8574e2b6f31e5fc131fd96cf711cca14bf4535d2
SuyashAgarwal14 <suyash140702@gmail.com>
2023-10-20 07:04:49 +0530

    Strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+quantity=10
+item_no=20
+price=100
+
+#fromatting string through format method
+bill="I want {} pieces of item {} for {:.2f} rupees".format(quantity,item_no,price)
+
+#fromatting string through f-strings
+bill=f"I want {quantity} pieces of item {item_no} for {price:.2f} rupees"
+
+name="""Suyash               
+Agarwal"""                  #used for multiple line string
+
+name2="""Amit\
+Agarwal"""
